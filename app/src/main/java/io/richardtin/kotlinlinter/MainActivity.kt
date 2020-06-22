7bc0829d045841b68ef580c3a92f02043d54343b
Richard.CW.Ting <richard.cw.ting@benq.com>
2020-06-22 08:45:35 +0800

    feat(ui): add an unselectable EditText

--- app/src/main/java/io/richardtin/kotlinlinter/MainActivity.kt
+++ app/src/main/java/io/richardtin/kotlinlinter/MainActivity.kt
@@ -1,11 +1,38 @@
 package io.richardtin.kotlinlinter
 
 import android.os.Bundle
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val input = findViewById<AppCompatEditText>(R.id.input)
+        input.disableCopyPaste()
+    }
+
+    private fun AppCompatEditText.disableCopyPaste() {
+        isLongClickable = false
+        setTextIsSelectable(false)
+        customSelectionActionModeCallback = object : ActionMode.Callback {
+            override fun onCreateActionMode(mode: ActionMode?, menu: Menu): Boolean {
+                return false
+            }
+
+            override fun onPrepareActionMode(mode: ActionMode?, menu: Menu): Boolean {
+                return false
+            }
+
+            override fun onActionItemClicked(mode: ActionMode?, item: MenuItem): Boolean {
+                return false
+            }
+
+            override fun onDestroyActionMode(mode: ActionMode?) {}
+        }
     }
 }
